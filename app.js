a97f1982678f4d9345293196ce22f1b5bf374071
TAHA <dg0882318@gmail.com>
2026-05-05 01:15:40 +0500

    Done

---
+++ app.js
@@ -0,0 +1,255 @@
+// ASSIGNMENT 2 JS 
+
+
+// 1- JAVASCRIPT BASICS
+
+
+// ANS1
+//  javascript is high level interpreted- programming language.
+
+// ANS2
+// . Game our animation kai liye [ Browser based game aur animation javascript sai create ki jati hai ]
+// . Website ki interactivity kai liye [ like slide , cart , button etc]
+// . Web- application bananai kai liye [ like complex app that handle social media ]
+
+// ANS3
+// console par jo kai chrome extension par javascript zyada tar use karta hai .
+
+// ANS4
+// Javascript interpreted language hai jo code ko line by line execute karta.
+
+// ANS5
+  var greet = "hello";
+  console.log(greet)
+
+
+
+// 2 - INTERPRETER VS COMPILER 
+
+// ANS1
+// interpreter program ko line by line read karke execute karta hai.
+
+// ANS2
+// Compiler pooray program ko aik sath run karta hai aur phir machine code mai convert karti hai 
+
+// ANS3
+//  Javascript ko browser ka engine line by line execute karta hai . 
+
+// ANS4
+// Difference :
+//  interpreter
+// .  line by line code execute hota.
+// .  error usi waqt show hojata hai.
+
+// compiler
+// . aik sath code execute hota.
+// . error compile honai kai baaad show hota.
+
+// ANS5
+//  c++ compiler user karti hai.
+
+
+
+// 3- VARIABLE VAR LET CONST    
+
+// ANS1
+// Variable ek container hota hai jisme hum data store karte hain.
+
+// ANS2 
+// var → old way, function scoped hota hai
+// let → modern, block scoped hota hai, value change ho sakti hai
+// const → block scoped hota hai, value change nahi hoti
+
+// ANS3
+// var ka scope function scope hota hai.
+
+// ANS4
+// var ka scope function scope hota hai.
+
+// ANS5
+// const tab use karte hain jab value change nahi karni ho (fixed value ho).
+
+// ANS6
+// Nahi, const variable ko dobara assign nahi kar sakte.
+
+// ANS7
+var x = 10;
+var x = 20;
+// 20
+console.log(x);
+
+// ANS8
+let y = 10;
+y = 15;
+// 15
+console.log(y);
+
+// ANS9 
+const z = 5;  // ERROR
+// z = 10;  // const mai dusri value use nhi hoti na hi chalti hai.
+
+
+
+//  4 - DATA TYPES 
+
+// ANS1
+// 2 type kai data type hotai hai 
+// premittive or non premittive data type 
+
+// ANS2
+  // PREMITTIVE DATA TYPE
+    // STRING
+    // NUMBER
+    // BOOLEAN 
+    // UNDEFINED
+    // NULL
+
+// ANS3
+// String text (words ya sentences) hota hai jo quotes mein likha jata hai.
+let name = "Ali";
+let city = "Karachi";
+
+// ANS4
+// Number woh hota hai jo mathematical values store karta hai.
+
+// ANS5
+// Boolean sirf 2 values leta hai:
+// true
+// false
+
+//  AMS5  
+//  Jab variable declare ho lekin value assign na ho.
+
+//  ANS6
+// Jab hum intentionally empty value set karte hain.
+
+// ANS7
+// Object ek collection hota hai key-value pairs ka.
+
+// ANS8 
+// Array ek list hoti hai values ki.
+let fruits = ["Apple", "Banana", "Mango"];
+
+
+
+// 5 - TYPE OF 
+
+// ANS1 
+//  type of operator wo hotai hai jo data type ko identify karke kai batatai ..
+
+// ANS2
+var meet = " hello"
+console.log(typeof meet)
+
+// ANS3
+let age = 20
+console.log(typeof age )
+
+// ANS4 
+let name1 = null;   //object 
+console.log(typeof name1)
+
+
+
+// 6- TRUTHY AND FALSY  
+
+// ANS1
+// truthy value sirf ture hoti hai..
+
+// ANS2
+  //  6 falsy value 
+    //  1 ""
+    //  2  false
+    // 3  not a number
+    // 4   0
+    // 5   undefined
+    // 6   null 
+
+
+// ANS3
+// false
+
+// ANS4
+// true
+
+// ANS5
+// false, 0, "" — kya yeh truthy hain ya falsy?
+// false false false 
+
+
+
+// 7-OPERATORS
+
+// ANS1
+    // arthimetic operator 
+    // +  -  * / % 
+
+// ANS2
+// 11 ANS 
+
+// ANS3
+//  =  // simple equal use for suming value.
+// ==    // data type check nhi kartta hai .
+
+// ANS4
+//  ===  // data type check karta hai.
+
+// ANS5
+//  5 == "5"  false kui kai data type dono different hai  
+
+
+
+// 8- COMPARISON OPERATOR 
+
+// ANS1
+     //  ==    // data type check nhi kartta
+    //  ===  // data type check karta hai
+    //  >    // greater than 
+    // <     // lewss than 
+    // >=    // greater than equal to 
+    // <=    // less than equal to 
+    // !    //  not 
+    // !=   //  not equal data type check nhi kartaa
+    // !==  //  not equal data type check karta hai 
+
+// AMS2
+// 10>5 true greater than 
+
+// ANS3
+// 10 === "10" false  data type different hai.
+
+
+
+
+// 9- LOGICAL OPERATOR 
+
+// ANS1
+// JavaScript mein 3 logical operators hote hain:
+// && → AND (aur)
+// || → OR (ya)
+// ! → NOT (ulta )
+
+// ANS2
+// true && false
+// output false 
+
+// ANS3
+// true || false
+// output true
+
+
+
+// 10- INCREMENT / DECREMENT
+
+// ANS1
+let c = 5;   // 5
+console.log(c++);
+
+// ANS2
+let q = 5;        // 6
+console.log(++q);
+    
+
+// ASSIGNMENT 2 COMPLETED
+
+
